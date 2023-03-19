2016361be7f15bd614c58886a59fd5dd6a4b87e1
Maxsman <dekxirus@gmail.com>
2023-03-19 19:50:17 +1000

    Add simple movement, StandaloneInputService and components for player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using Scriptable.Move;
+using Services.InputService;
+using UnityEngine;
+using Zenject;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Inject] private IInputService _inputService;
+        [Inject] private Movement _movement;
+
+        private Rigidbody2D _rigidbody2D;
+
+        public void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        public void Update()
+        {
+            _movement.Move(_inputService.Axis, _rigidbody2D);
+        }
+    }
+}
